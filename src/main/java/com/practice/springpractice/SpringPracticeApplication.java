8c7d90bd1e2ccf957057a6cea6f6e64967c4cf61
karkig <kailash.karki4@gmail.com>
2023-07-24 16:43:00 +0530

    config changes

--- src/main/java/com/practice/springpractice/SpringPracticeApplication.java
+++ src/main/java/com/practice/springpractice/SpringPracticeApplication.java
@@ -2,11 +2,13 @@ package com.practice.springpractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableEurekaClient
 @ComponentScan("com.practice.springpractice")
 public class SpringPracticeApplication {
 
